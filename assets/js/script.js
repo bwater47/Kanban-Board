545652a8883bdb9688a61fe83a84a07300d91a2c
B Water <brennw47@gmail.com>
2024-04-01 15:50:44 -0600

    Update script.js
    Updated the script.js to have an event listener for the btn, and added a function that picks a date in the modal containing a form to add tasks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,27 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
   // Generate a unique task ID using timestamp and random number
-  const date = "2019-01-25";
+  const date = "2024-04-01";
   const unixTimestamp = dayjs(date).unix();
-//   const timestamp = new Date().getTime(); // Current timestamp
+  //   const timestamp = new Date().getTime(); // Current timestamp
   const randomNumber = Math.floor(Math.random() * 9001); // Random number between 0 and 9999
   const uniqueId = unixTimestamp + "-" + randomNumber; // Combine timestamp and random number
   return uniqueId;
-//   console.log(uniqueId);
+  //   console.log(uniqueId);
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-  // Create a task card using an event listener on add task, a form to populate with title, datepicker, description.
+  // Create a task card using an event listener on add task, a form to populate with Task Title, Task Due Date, Task Description.
+  task.addEventListener("btn", {modal});
   // #formModal for datepicker
   // modal html line 25 for title, and description.
+  $(function () {
+    $("#formModal").datepicker({
+      changeMonth: true,
+      changeYear: true,
+    });
+  });
 }
 
 // Todo: create a function to render the task list and make cards draggable
