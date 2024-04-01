624ca5ea868035b72f8c54ac627b98608c7eb52f
B Water <brennw47@gmail.com>
2024-04-01 14:47:02 -0600

    Update script.js
    Updated script.js to have a function that generates a new id called unique id which uses day.js to grab a unix timestamp using a date, and then the math.random function to generate a random number then combine them with a sum to return a unique id.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,35 +4,34 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+  // Generate a unique task ID using timestamp and random number
+  const date = "2019-01-25";
+  const unixTimestamp = dayjs(date).unix();
+//   const timestamp = new Date().getTime(); // Current timestamp
+  const randomNumber = Math.floor(Math.random() * 9001); // Random number between 0 and 9999
+  const uniqueId = unixTimestamp + "-" + randomNumber; // Combine timestamp and random number
+  return uniqueId;
+//   console.log(uniqueId);
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+  // Create a task card using an event listener on add task, a form to populate with title, datepicker, description.
+  // #formModal for datepicker
+  // modal html line 25 for title, and description.
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
-}
+function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
-}
+function handleAddTask(event) {}
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
+function handleDeleteTask(event) {}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
+function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
-
-});
+$(document).ready(function () {});
