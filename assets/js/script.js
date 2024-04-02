8cde7c54ba6f8d1ecc01d430b1ee8ff1b7056c8f
B Water <brennw47@gmail.com>
2024-04-01 22:14:07 -0600

    Update script.js/index.html
    Updated the script.js to have a variable for the submit button/must add the id, added a taskTitleInput.value for a tasks array, and added an on click call the handleAddTask function. Updated the index.html file to have a new button layout for the Add Task button, and added a modal with a form inside of it, and changed the classes for the labels as well as what they're for, will change them so they match up to javascript.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const submitButton = document.querySelector('#submitButton');
+
+
+
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -37,17 +41,33 @@ function createTaskCard(task) {
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {}
+function renderTaskList() {
+
+}
 // Render three columns into the dom, iterate over taskList to place the tasks created in the correct columns.
+
 // Todo: create a function to handle adding a new task
-function handleAddTask(event) {}
-// Add function to add taskCard when we click submit. Push to an array so it persists
+function handleAddTask(event) {
+
+    const tasks = {
+        taskTitle: taskTitleInput.value
+    }
+}
+// Add function to add taskCard when we click submit. Push to an array so it persists.
+
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event) {}
+function handleDeleteTask(event) {
+    
+}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {}
+function handleDrop(event, ui) {
+
+}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {});
+$(document).ready(function () {
+$(submitButton).on('click', handleAddTask)
+});
+
 // Take a task and create a card from the task.
