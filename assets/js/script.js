50532e90f2fb714192b94527c4a12d5a45404ee8
B Water <brennw47@gmail.com>
2024-04-02 21:47:53 -0600

    Update script.js/index.html
    Updated the index.html to have new comments, and updated the script.js to comment out the const declarations at on lines 4-8, line 41 is now seperated to lines 47-49, removed line 67 and now the function for the date picker is inside the document.ready function on line 72. However I will work on adding the rest with my tutor so I can get some notes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-const openButton = document.querySelector("#formModal");
-const submitButton = document.querySelector(".modal-body");
-const taskTitleInput = document.querySelector("#taskTitle");
-const taskDueDateInput = document.querySelector("#taskDueDate");
-const taskDescriptionInput = document.querySelector("#taskDescription");
+// const openButton = document.querySelector("#formModal");
+// const submitButton = document.querySelector(".modal-body");
+// const taskTitleInput = document.querySelector("#taskTitle");
+// const taskDueDateInput = document.querySelector("#taskDueDate");
+// const taskDescriptionInput = document.querySelector("#taskDescription");
 
+// const tasks = {
+  //   taskTitle: taskTitleInput.value,
+  //   taskDueDate: taskDueDateInput.value,
+  //   taskDescription: taskDescriptionInput.value,
+  // };
+  // console.log(tasks);
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
   return Date.now();
@@ -38,33 +44,35 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-  $("#todo-cards, #in-progress-cards, #done-cards").empty();
+  $("#todo-cards").empty();
+  $("#in-progress-cards").empty();
+  $("#done-cards").empty();
 
   taskList.forEach(task) => {
     let card = createTaskCard(task);
-  // const tasks = {
-  //   taskTitle: taskTitleInput.value,
-  //   taskDueDate: taskDueDateInput.value,
-  //   taskDescription: taskDescriptionInput.value,
-  // };
-  // console.log(tasks);
+
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event) {}
+function handleAddTask(event) {
+  
+}
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event) {}
+function handleDeleteTask(event) {
+
+}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {}
+function handleDrop(event, ui) {
+
+}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
   $(submitButton).on("click", handleAddTask);
-});
 
-$(function () {
+
   $("#taskDueDate").datepicker({
     changeMonth: true,
     changeYear: true,
