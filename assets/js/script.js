e0f67b66a7aafd1aa7d9e578f8b1d90f222dc5e0
B Water <brennw47@gmail.com>
2024-04-02 18:28:00 -0600

    Update script.js/index.html
    Updated the script.js to include less comments, i'm getting a tutor session soon here so I'll add comments then. Updated the index.html to have a new class on line 72,  then changed the card body colors on line 66, and 76.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,44 +10,30 @@ const taskDescriptionInput = document.querySelector("#taskDescription");
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
   return Date.now();
-  //   const date = "2024-04-01";
-  //   const unixTimestamp = dayjs(date).unix();
-  //   const timestamp = new Date().getTime(); // Current timestamp
-  // const randomNumber = Math.floor(Math.random() * 9002);
-  // const uniqueId = unixTimestamp + "-" + randomNumber;
-  // return uniqueId;
-  // localStorage.setItem('uniqueId')
-  //   console.log(uniqueId);
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-  const tasks = {
-    taskTitle: taskTitleInput.value,
-    taskDueDate: taskDueDateInput.value,
-    taskDescription: taskDescriptionInput.value,
-  };
-  console.log(tasks);
   $(function () {
     $("#taskDueDate").datepicker({
       changeMonth: true,
       changeYear: true,
     });
   });
-  // Capture variables to use in the script from form, and render it to the screen in the #to-do.
-  // Use day.js here to locally store the unix date.
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-  // Create a task card using an event listener on add task, 
-  // a form to populate with Task Title, Task Due Date, Task Description.
+  const tasks = {
+    taskTitle: taskTitleInput.value,
+    taskDueDate: taskDueDateInput.value,
+    taskDescription: taskDescriptionInput.value,
+  };
+  console.log(tasks);
 }
-// Render three columns into the dom, iterate over taskList to place the tasks created in the correct columns.
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event) {}
-// Add function to add taskCard when we click submit. Push to an array so it persists.
 
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event) {}
